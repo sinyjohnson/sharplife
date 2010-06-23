e06b3d6d74c0de718268bce89db0fac763d82469
woz1010 <woz1010@e4e13283-53c3-3793-2e2f-77bc850ac39f>
2010-06-23 18:12:23 +0000

    Added Apache License 2.0 to source files

--- trunk/SpeedTestConsole/Properties/AssemblyInfo.cs
+++ trunk/SpeedTestConsole/Properties/AssemblyInfo.cs
@@ -1,11 +1,29 @@
-﻿using System.Reflection;
+﻿#region Apache License 2.0
+/*
+   Copyright 2010 SF Games
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpeedTestConsole")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tests speed of the various Life engines")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SF Games")]
 [assembly: AssemblyProduct("SpeedTestConsole")]
